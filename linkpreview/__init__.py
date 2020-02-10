7f550173211f8c2cdd6d43d5f06beb6ee115ad4c
meyt <pasd3000@gmail.com>
2020-02-10 21:19:58 +0330

    v0.1.2

--- linkpreview/__init__.py
+++ linkpreview/__init__.py
@@ -4,6 +4,6 @@ from .preview import LinkPreview
 from .compose import link_preview
 from .exceptions import LinkPreviewException
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = (Link, LinkGrabber, LinkPreview, link_preview, LinkPreviewException)
