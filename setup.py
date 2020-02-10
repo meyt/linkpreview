b09b6fec89817a266aaf58b771493418ac1a26ef
meyt <pasd3000@gmail.com>
2020-02-10 22:55:35 +0330

    Update CI

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries",
     ],
     packages=find_packages(),
